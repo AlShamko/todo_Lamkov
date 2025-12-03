f3c8c6cb4b90e26dae94b140e9ed0c696b80f716
Aliaksandr Shamko <shamkoag@gmail.com>
2025-12-03 10:22:19 +0100

    feat: add url my rep in vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: '/https://alshamko.github.io/todo_Lamkov/',
+    base: '/todo_Lamkov/',
 })
